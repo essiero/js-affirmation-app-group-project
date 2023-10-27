314e2fc3094519af34ed87260ee068b34ffc5b40
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-10-27 11:06:11 -0500

    got affirmations and authors to add and remove to table and emojis to append and remove

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 onReady();
 
+function handleSubmit(event){
+  event.preventDefault();
+  console.log(`It worked!`);
+  let affirmation = document.getElementById('aff-text').value;
+  document.getElementById('aff-text').value = ''
+  let author = document.getElementById('aff-author').value;
+  document.getElementById('aff-author').value = '';
+  tableBody.innerHTML +=
+  `<tr>
+  <td>${affirmation}</td>
+  <td>${author}</td>
+  <td><button onclick="deleteAff(event)">🆒</button></td>
+</tr>`
+}
+
+function deleteAff(event){
+  event.target.parentElement.parentElement.remove();
+}
+
+function onFireClick() {
+  console.log(`We're in the fireClick function!`)
+  let fireClick = document.getElementById("emojiBin");
+  console.log(fireClick);
+  emojiBin.innerHTML += `<h2>🔥</h2>`
+}
+
+function onIceClick() {
+  emojiBin.innerHTML += `<h2>❄️</h2>`
+}
+
 function onReady() {
   console.log('Javascript is working!');
 }
